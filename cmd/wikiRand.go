9e3d7e77f21e97d45109805d8d076a3e1b55975c
rubensseva <r.sevaldson@gmail.com>
2019-07-23 07:31:49 +0200

    CHANGE: Some fixes

--- cmd/wikiRand.go
+++ cmd/wikiRand.go
@@ -1,35 +1,37 @@
 /*
-Copyright © 2019 Ruben Svanåsbakken Sevaldson <r.sevaldson@gmail.com>
+	Copyright © 2019 Ruben Svanåsbakken Sevaldson <r.sevaldson@gmail.com>
 
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
+	Permission is hereby granted, free of charge, to any person obtaining a copy
+	of this software and associated documentation files (the "Software"), to deal
+	in the Software without restriction, including without limitation the rights
+	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+	copies of the Software, and to permit persons to whom the Software is
+	furnished to do so, subject to the following conditions:
 
-The above copyright notice and this permission notice shall be included in
-all copies or substantial portions of the Software.
+	The above copyright notice and this permission notice shall be included in
+	all copies or substantial portions of the Software.
 
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-THE SOFTWARE.
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+	THE SOFTWARE.
 */
 
 // Package cmd represents cobra command
 package cmd
 
 import (
+	"bufio"
 	"fmt"
 	"io/ioutil"
 	"net/http"
 	"strings"
 
 	"github.com/spf13/cobra"
+	"golang.org/x/net/html"
 )
 
 type jsonMapper struct {
@@ -41,7 +43,7 @@ var wikiRandCmd = &cobra.Command{
 	Use:   "wikiRand",
 	Short: "Play with random wikipedia artivle",
 	Long: `Pulls a random article from wikipedia
-	and plays it immediatly`,
+		and plays it immediatly`,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("wikiRand called")
 
@@ -67,6 +69,21 @@ var wikiRandCmd = &cobra.Command{
 		bytes, _ = ioutil.ReadAll(resp.Body)
 
 		fmt.Println("HTML:\n\n", string(bytes))
+		r := bufio.NewReader(bytes)
+		z := html.NewTokenizer(r)
+		for {
+			tt = z.Next()
+			switch tt {
+			case html.ErrorToken:
+				return z.Err()
+			case html.TextToken:
+				if depth > 0 {
+					// emitBytes should copy the []byte it receives,
+					// if it doesn't process it immediately.
+					emitBytes(z.Text())
+				}
+			}
+		}
 	},
 }
 
